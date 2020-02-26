3452b8c0ad9dc3a7cf20930037ec3037ca3aca1a
Oliver Weber <oliver@mondough.io>
2020-02-26 15:03:24 +0100

    attacker and attacker with progress

--- attacker-withProgress.js
+++ attacker-withProgress.js
@@ -3,16 +3,27 @@ const axios = require("axios");
 const { pinifyNumber } = require("./lib/randomPin");
 const { maxPin } = require("./config");
 
+// Optional Progressbar
+const cliProgress = require("cli-progress");
+const progress = new cliProgress.SingleBar(
+  {},
+  cliProgress.Presets.shades_classic
+);
+
 async function main() {
+  progress.start(maxPin, 0);
+
   for (let i = 0; i < maxPin; i++) {
     const tryPin = pinifyNumber(i);
     const { data } = await axios.get(`http://localhost:3000/${tryPin}`);
 
+    progress.increment();
+
     if (data) {
+      progress.stop();
+      console.log("\u001b[2J\u001b[0;0H");
       console.log(
-        `
-        Success the pin is: ${pinifyNumber(i, 4)}
-        failed attempts:  ${i - 1}
+        `Success the pin is: ${pinifyNumber(i, 4)} \n failed attempts:  ${i - 1}
         `
       );
 
